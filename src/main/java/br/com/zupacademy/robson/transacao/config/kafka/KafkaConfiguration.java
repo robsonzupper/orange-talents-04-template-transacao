8ef1037bc79dbabc903adc45e1d0fd8943bbaffc
robsonzupper <robson.diniz@zup.com.br>
2021-05-31 06:17:32 -0300

    feat: create kafka configuration

---
+++ src/main/java/br/com/zupacademy/robson/transacao/config/kafka/KafkaConfiguration.java
@@ -0,0 +1,66 @@
+package br.com.zupacademy.robson.transacao.config.kafka;
+
+import java.util.Map;
+import java.util.HashMap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.context.annotation.Configuration;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import br.com.zupacademy.robson.transacao.transaction.TransactionEvent;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+
+@Configuration
+public class KafkaConfiguration {
+
+    private final KafkaProperties kafkaProperties;
+
+    public KafkaConfiguration(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    public Map<String, Object> consumerConfigurations() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaProperties.getBootstrapServers());
+
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                kafkaProperties.getConsumer().getKeyDeserializer());
+
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                kafkaProperties.getConsumer().getValueDeserializer());
+
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG,
+                kafkaProperties.getConsumer().getGroupId());
+
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                kafkaProperties.getConsumer().getAutoOffsetReset());
+
+        return properties;
+    }
+
+    @Bean
+    public ConsumerFactory<String, TransactionEvent> transactionConsumerFactory() {
+        StringDeserializer stringDeserializer = new StringDeserializer();
+        JsonDeserializer<TransactionEvent> jsonDeserializer
+                = new JsonDeserializer<>(TransactionEvent.class, false);
+
+        return new DefaultKafkaConsumerFactory<>(consumerConfigurations(),
+                stringDeserializer, jsonDeserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, TransactionEvent>
+            kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, TransactionEvent> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(transactionConsumerFactory());
+
+        return factory;
+    }
+
+}
